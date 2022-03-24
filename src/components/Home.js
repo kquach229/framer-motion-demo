cea886a24b65a6e842b699196b8c5f5e2b492450
kquach229 <kquach229>
2022-03-23 22:47:39 -0400

    save

---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1, 
+    textShadow: "0px 0px 8px rgb(255,255,255)", 
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3
+    }
+  }
+}
+
+const Home = () => {
+  return (
+    <motion.div 
+      animate={{ opacity: 1 }} 
+      initial={{ opacity: 0 }} 
+      transition={{ delay: 1.5, duration: 1.5 }}
+      className="home container">
+      <h2 animate={{ }}>
+        Welcome to Pizza Joint
+      </h2>
+      <Link to="/base">
+        <motion.button 
+          variants={buttonVariants}
+          whileHover="hover">
+          Create Your Pizza
+        </motion.button>
+      </Link>
+    </motion.div>
+  )
+}
+
+export default Home;
